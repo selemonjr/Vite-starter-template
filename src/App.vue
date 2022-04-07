bf46aee525280f1d1a9cd9c07af792a4b4d842d4
selemonjr <selemonjr@gmail.com>
2022-04-07 12:01:11 -0700

    hover-effect

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,6 @@
-<template>
-  <div>
-    <Navigation/>
-        <router-view/>
-  </div>
-</template>
-<script>
-import Navigation from "./components/Navigation.vue"
-export default {
-  name:"App",
-  components: {
-    Navigation
-  }
-}
+<script setup>
+
 </script>
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<template>
+<router-view/>
+</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,58 @@
-<template>
-    <div>
-        <h2 class="text-2xl font-bold font-serif">Welcome To Vite starter template</h2>
-    </div>
-</template>
-<script>
-export default {
-    name:"Home",
-}
+
+<script setup>
+
 </script>
+<template>
+<div class="bg-black relative min-h-screen">
+    <section class="flex flex-col md:flex-row justify-center items-center pt-40 gap-6 px-8 max-w-[1400px] mx-auto">
+      <div class="brand group">
+        <img src="../assets/disney.png" layout="fill" objectFit="cover" />
+             <video  loop autoplay playsInline class="hidden group-hover:inline rounded-lg object-cover absolute">
+          <source src="../assets/disney.mp4" type="video/mp4" />
+        </video>
+      </div>
+
+      <div class="brand group">
+        <img src="../assets/pixar.png" layout="fill" objectFit="cover" />
+        <video  loop autoplay playsInline class="hidden group-hover:inline rounded-lg object-cover absolute">
+          <source src="../assets/pixar.mp4" type="video/mp4" />
+        </video>
+      </div>
+
+      <div class="brand group">
+        <img src="../assets/marvel.png" layout="fill" objectFit="cover" />
+        <video
+           loop autoplay playsInline
+          class="hidden group-hover:inline rounded-lg object-cover absolute"
+        >
+          <source src="../assets/marvel.mp4" type="video/mp4" />
+        </video>
+      </div>
+
+      <div class="brand group">
+        <img src="../assets/starwars.png" layout="fill" objectFit="cover" />
+        <video
+           loop autoplay playsInline
+          class="hidden group-hover:inline rounded-lg object-cover absolute"
+        >
+          <source src="../assets/star-wars.mp4" type="video/mp4" />
+        </video>
+      </div>
+
+      <div class="brand group">
+        <img
+          src="../assets/national-geographic.png"
+          layout="fill"
+          objectFit="cover"
+          class="absolute"
+        />
+        <video
+           loop autoplay playsInline
+          class="hidden group-hover:inline rounded-lg object-cover absolute"
+        >
+          <source src="../assets/national-geographic.mp4" type="video/mp4" />
+        </video>
+      </div>
+    </section>
+</div>
+</template>
