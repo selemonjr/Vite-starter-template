26120c8005a4b8e86b7753b64edd889f987ba35f
selemondev <selemondev19@gmail.com>
2022-06-10 10:23:42 +0300

    vuex replaced

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,58 +1,14 @@
-
 <script setup>
-
+import { useStore } from "../store/index.js";
+const store = useStore();
 </script>
 <template>
-<div class="bg-black relative min-h-screen">
-    <section class="flex flex-col md:flex-row justify-center items-center pt-40 gap-6 px-8 max-w-[1400px] mx-auto">
-      <div class="brand group">
-        <img src="../assets/disney.png" layout="fill" objectFit="cover" />
-             <video  loop autoplay playsInline class="hidden group-hover:inline rounded-lg object-cover absolute">
-          <source src="../assets/disney.mp4" type="video/mp4" />
-        </video>
-      </div>
-
-      <div class="brand group">
-        <img src="../assets/pixar.png" layout="fill" objectFit="cover" />
-        <video  loop autoplay playsInline class="hidden group-hover:inline rounded-lg object-cover absolute">
-          <source src="../assets/pixar.mp4" type="video/mp4" />
-        </video>
-      </div>
-
-      <div class="brand group">
-        <img src="../assets/marvel.png" layout="fill" objectFit="cover" />
-        <video
-           loop autoplay playsInline
-          class="hidden group-hover:inline rounded-lg object-cover absolute"
-        >
-          <source src="../assets/marvel.mp4" type="video/mp4" />
-        </video>
-      </div>
-
-      <div class="brand group">
-        <img src="../assets/starwars.png" layout="fill" objectFit="cover" />
-        <video
-           loop autoplay playsInline
-          class="hidden group-hover:inline rounded-lg object-cover absolute"
-        >
-          <source src="../assets/star-wars.mp4" type="video/mp4" />
-        </video>
-      </div>
-
-      <div class="brand group">
-        <img
-          src="../assets/national-geographic.png"
-          layout="fill"
-          objectFit="cover"
-          class="absolute"
-        />
-        <video
-           loop autoplay playsInline
-          class="hidden group-hover:inline rounded-lg object-cover absolute"
-        >
-          <source src="../assets/national-geographic.mp4" type="video/mp4" />
-        </video>
-      </div>
-    </section>
+<div class="grid place-items-center">
+      <p class="font-bold mt-10 text-xl">{{store.counter}}</p>
+  <div class="flex justify-center items-center">
+     <button @click="store.increment" class="btn bg-green-500">Increment</button>
+      <button @click="store.decrement" class="btn bg-red-500">Decrement</button>
+       <button @click="store.reset" class="btn bg-red-500">Reset</button>
+  </div>
 </div>
 </template>
